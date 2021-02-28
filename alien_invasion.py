811766d633236b021349b08d639fd7b9a19b5a77
MrsRector <rectortrish@gmail.com>
2021-02-27 19:08:34 -0800

    Flle commits for ai_file, settings, and bullet files.

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+
 
 
 
@@ -19,6 +21,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode ((self.settings.screen_width, self.settings.screen_height))      
         pygame.display.set_caption ("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         
         
 
@@ -28,6 +31,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
 
@@ -56,6 +60,14 @@ class AlienInvasion:
                 pygame.quit()
                 sys.exit()
 
+            elif event.key == pygame.K_SPACE:
+                self._fire_bullet()
+
+    def _fire_bullet(self):
+        #Create a new bullet and add it to the bullets group.
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
          
 
     def _check_keyup_events (self, event):
@@ -73,6 +85,8 @@ class AlienInvasion:
 
         self.screen.fill (self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         pygame.display.flip()
 
